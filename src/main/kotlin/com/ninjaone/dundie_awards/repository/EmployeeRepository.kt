0f79dfe7823c18314775a57f719e2e640794d48b
Phellipe Ribeiro <3440523+pkelbert@users.noreply.github.com>
2026-01-17 13:56:35 -0300

    Fix employee repository

--- src/main/kotlin/com/ninjaone/dundie_awards/repository/EmployeeRepository.kt
+++ src/main/kotlin/com/ninjaone/dundie_awards/repository/EmployeeRepository.kt
@@ -1,6 +1,8 @@
 package com.ninjaone.dundie_awards.repository
 
+import com.ninjaone.dundie_awards.model.Employee
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface EmployeeRepository
+interface EmployeeRepository : JpaRepository<Employee, Long>
